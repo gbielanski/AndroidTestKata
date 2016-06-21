8d3fa8d6750398905cce8931d575cbe2ecabe512
Grzegorz Bielanski <gbielanski@poczta.onet.pl>
2016-06-21 10:16:04 +0200

    Create DialogFragment class

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 1. Create Project
 2. Add Button to design
 3. Add Butterknife to compilation and init it
+4. Create DialogFragment class
---
+++ app/src/main/java/pl/kata/bielanski/androidtestkata/TestDialog.java
@@ -0,0 +1,18 @@
+package pl.kata.bielanski.androidtestkata;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TestDialog extends DialogFragment {
+
+	@Nullable
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		return super.onCreateView(inflater, container, savedInstanceState);
+	}
+}
